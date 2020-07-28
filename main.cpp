22df73bdc9dac3ceab74e5332bbdf253c7649af0
Abdalla Atalla <abdalla@abdallas-mbp.attlocal.net>
2020-07-28 12:38:52 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,69 @@
+//
+//  main.cpp
+//  ExtraCredit
+//  This program is written by Abdalla Atalla and finished on 11/ /19. This program asks the user to input numbers from the keyboard and then it will find the average of the maximum and minimum values from that set of numeric values. The program will then continue to run until the user decides he wants to quit.
+//  Created by Abdalla Atalla on 11/7/19.
+//  Copyright © 2019 Abdalla Atalla. All rights reserved.
+//
+
+#include <iostream>
+#include <list>
+#include <cmath>
+
+using namespace std;
+
+
+float formatNumber(double d) {
+    return std::ceil(d * 100.00) / 100.00;
+}
+
+void showMaxMin(list<double> l) {
+    double maxNumber = l.front();
+    double minNumber = l.front();
+    
+    list<double>::iterator it;
+    for (it = l.begin(); it != l.end(); ++it) {
+        if (*it > maxNumber) {
+            maxNumber = *it;
+        }
+        
+        if (*it < minNumber) {
+            minNumber = *it;
+        }
+    }
+    cout << "Maximum number is: " << maxNumber << endl;
+    cout << "Minimum number is: " << minNumber << endl;
+    cout << "Average is: " << formatNumber((maxNumber + minNumber)/2) << endl;
+    cout << endl;
+}
+
+int main() {
+    
+    list<double> myList;
+    
+    double number = -1;
+    string existYN;
+    
+    
+    do {
+        cout << "Please enter your number to the list: ";
+        
+        if (cin >> number) {
+            myList.push_back(formatNumber(number));
+        } else {
+            cout << "Invalid input! Please enter a valid number!" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            
+        }
+        cout << "Are you done? (Y/N) " ;
+        cin >> existYN;
+            
+    } while (existYN != "Y" and existYN != "y");
+    
+    
+    showMaxMin(myList);
+
+    myList.clear();
+    return 0;
+}
